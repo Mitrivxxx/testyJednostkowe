52d3225720b1a485b14e33aced6bcc64f202bd47
Mateusz <matsma2468@gmail.com>
2024-11-28 21:53:20 +0100

    Sortowanie mergeSort, wyswietlanie posortowanej tablicy

---
+++ MergeSort.h
@@ -0,0 +1,10 @@
+class MergeSort {
+public:
+    void sort(int arr[], int size);
+    void mergeSort(int arr[], int left, int right);
+    void merge(int arr[], int left, int mid, int right);
+    void showArray(int arr[], int size);
+};
+
+
+
---
+++ main.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "MergeSort.h"
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+        int arr[] = { 13, 4, 17, 9, 0, 12, 19, 8, 6, 18, 7, 3, 14, 1, 15, 10, 11, 2, 5, 20, 16};
+        int size = sizeof(arr) / sizeof(arr[0]);
+
+        MergeSort sorter;
+        sorter.showArray(arr, size);
+
+        sorter.sort(arr, size);
+
+        cout << "Tablica po sortowaniu: ";
+        sorter.showArray(arr, size);
+
+    return 0;
+}
