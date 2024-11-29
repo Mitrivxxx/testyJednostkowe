0c0e0eb750d5303c3ef061f711006449652dc63a
Mateusz <matsma2468@gmail.com>
2024-11-29 13:37:39 +0100

    test dodatnie i ujemne

--- test2.cpp
+++ test2.cpp
@@ -28,8 +28,20 @@ TEST(czy_poprawnie_sortuje_losowa_tablice, ArraySortedCorrectly) {
 //poprawnie sortuje tablice tylko z liczbami ujemnymi,
 TEST(czy_poprawnie_sortuje_z_liczbami_ujemnymi, ArraySortedCorrectly) {
     MergeSort sorter;
-    int arr[] = { -4, -5, -1, -2, -3 };
+    int arr[] = { -1, -3, -2, -5, -4 };
     int orginalArray[] = { -5,-4,-3,-2,-1 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    sorter.sort(arr, size);
+    sorter.showArray(arr, size);
+    for (int i = 0; i < size; ++i) {
+        EXPECT_EQ(arr[i], orginalArray[i]);
+    }
+}
+//poprawnie sortuje tablice z liczbami ujemnymi i liczbami dodatnimi
+TEST(czy_poprawnie_sortuje_z_liczbami_ujemnymi_i_dodatnimi, ArraySortedCorrectly) {
+    MergeSort sorter;
+    int arr[] = { -1, 3, 2, -5, -4 };
+    int orginalArray[] = { -5,-4,-1, 2, 3 };
     int size = sizeof(arr) / sizeof(arr[0]);
     sorter.sort(arr, size);
 
