4aeedee78b33a8f5bc84767db95822df738686f4
Mateusz <matsma2468@gmail.com>
2024-11-29 13:30:33 +0100

    test 4

--- test2.cpp
+++ test2.cpp
@@ -16,8 +16,20 @@ TEST(czy_poprawana_w_poprawnej_kolejnosci, ArraySortedCorrectly) {
 //Poprawnie sortuje losow¹ tablicê liczb
 TEST(czy_poprawnie_sortuje_losowa_tablice, ArraySortedCorrectly) {
     MergeSort sorter;
-    int arr[] = { 4,2,5,1,3 };
+    int arr[] = { 4, 5, 1, 2, 3 };
     int orginalArray[] = { 1,2,3,4,5 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    sorter.sort(arr, size);
+
+    for (int i = 0; i < size; ++i) {
+        EXPECT_EQ(arr[i], orginalArray[i]);
+    }
+}
+//poprawnie sortuje tablice tylko z liczbami ujemnymi,
+TEST(czy_poprawnie_sortuje_z_liczbami_ujemnymi, ArraySortedCorrectly) {
+    MergeSort sorter;
+    int arr[] = { -4, -5, -1, -2, -3 };
+    int orginalArray[] = { -5,-4,-3,-2,-1 };
     int size = sizeof(arr) / sizeof(arr[0]);
     sorter.sort(arr, size);
 
