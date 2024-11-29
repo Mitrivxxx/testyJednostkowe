36b5ae48c6bf61dbdb5a07533a4c2fcdcaab25b9
Mateusz <matsma2468@gmail.com>
2024-11-29 13:25:24 +0100

    test 3

--- test2.cpp
+++ test2.cpp
@@ -13,3 +13,15 @@ TEST(czy_poprawana_w_poprawnej_kolejnosci, ArraySortedCorrectly) {
         EXPECT_EQ(arr[i], orginalArray[i]);
     }
 }
+//Poprawnie sortuje losow¹ tablicê liczb
+TEST(czy_poprawnie_sortuje_losowa_tablice, ArraySortedCorrectly) {
+    MergeSort sorter;
+    int arr[] = { 4,2,5,1,3 };
+    int orginalArray[] = { 1,2,3,4,5 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    sorter.sort(arr, size);
+
+    for (int i = 0; i < size; ++i) {
+        EXPECT_EQ(arr[i], orginalArray[i]);
+    }
+}
